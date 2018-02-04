01b4364e47799f2215acb043f0eebec8e27826e6
Paweł <pawel.wojtasinski.1995@gmail.com>
2018-02-04 12:45:21 +0100

    Initialize commit

---
+++ app/containers/App.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+
+class App extends Component {
+    render() {
+        return (
+            <div>
+                Text
+            </div>
+        )
+    }
+}
+export default App
