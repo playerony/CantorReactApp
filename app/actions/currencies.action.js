d7bda713c46d23dd959a896e63b764364c91dfbc
Paweł <pawel.wojtasinski.1995@gmail.com>
2018-02-04 18:07:53 +0100

    Create action for currency

---
+++ app/actions/currencies.action.js
@@ -0,0 +1,31 @@
+import {
+    REQUEST_CURRENCIES,
+    RECEIVE_CURRENCIES,
+    FAILURE_CURRENCIES
+} from '../constants/currencies.constants'
+import * as currenciesService from '../services/currencies.service.js'
+
+function requestCurrencies() {
+  return {
+    type: REQUEST_CURRENCIES
+  }
+}
+
+function receiveCurrencies(json) {
+  console.log(json);
+
+  return {
+    type: RECEIVE_CURRENCIES,
+    currencies: json,
+    receivedAt: Date.now()
+  }
+}
+
+export function fetchCurrencies() {
+  return dispatch => {
+    currenciesService.fetchCurrencies()
+    .then(
+        json => dispatch(receiveCurrencies(json))
+    );
+  }
+}
