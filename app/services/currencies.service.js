6799cdd592f9f26e34d64da68d9f18d6fb91c654
Paweł <pawel.wojtasinski.1995@gmail.com>
2018-02-04 16:23:58 +0100

    Create currencies service

---
+++ app/services/currencies.service.js
@@ -0,0 +1,10 @@
+import fetch from 'cross-fetch'
+
+let apiUrl = 'http://webtask.future-processing.com:8068'
+
+export function fetchCurrencies() {
+    return fetch(apiUrl + '/currencies')
+        .then(response => {
+            return response.json()
+        })
+}
