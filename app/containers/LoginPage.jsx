dd380723f16ac6b1f16a0d86f8954ce67eacbf28
Paweł <pawel.wojtasinski.1995@gmail.com>
2018-02-18 19:46:31 +0100

    Create basic LoginPage

---
+++ app/containers/LoginPage.jsx
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+
+class LoginPage extends Component {
+    constructor(props) {
+        super(props)
+    }
+
+    render() {
+        return (
+            <div>
+                Login page
+            </div>
+        )
+    }
+}
+export default LoginPage
