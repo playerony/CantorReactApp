7d949e38c447b9e8b9d56c14da0ad1530ebe21d5
Paweł <pawel.wojtasinski.1995@gmail.com>
2018-02-24 17:38:30 +0100

    Create EditPage class for update user

---
+++ app/containers/EditPage.jsx
@@ -0,0 +1,84 @@
+import React, { Component } from 'react'
+import { connect } from 'react-redux'
+import { Redirect } from 'react-router'
+import {
+    Link
+} from 'react-router-dom'
+
+import {
+    updateUser
+} from '../actions/user/update.action.js'
+import UserForm from '../components/UserForm.jsx'
+
+class EditPage extends Component {
+    constructor(props) {
+        super(props)
+
+        this.state = {
+            user: {
+                userId: this.props.payload.id,
+                firstName: this.props.payload.firstName,
+                lastName: this.props.payload.lastName,
+                username: this.props.payload.username,
+                password: '',
+                email: this.props.payload.email,
+                balance: 0,
+                roleId: 1
+            }
+        }
+
+        this.handleSubmit = this.handleSubmit.bind(this)
+    }
+
+    handleSubmit(editForm, event) {
+        event.preventDefault()
+
+        const { dispatch } = this.props
+        editForm.userId = payload.id
+        dispatch(updateUser(editForm))
+    }
+
+    render() {
+        const { alert, isFetching, isError, isRegistered } = this.props
+
+        return (
+            <div>
+                <UserForm user={this.state.user}
+                          alert={alert}
+                          isFetching={isFetching}
+                          isError={isError}
+                          isRegistered={isRegistered}
+                          title="Edit Page"
+                          buttonText="Edit Account"
+                          redirectPage = "/home"
+                          onSubmit={this.handleSubmit} />
+            </div>
+        )
+    }
+}
+
+function mapStateToProps(state) {
+    const { alert, editUser, fetchUser } = state
+
+    const {
+        isFetching,
+        isError,
+        isRegistered
+    } = editUser || {
+        isFetching: true,
+        isError: false
+    }
+
+    const {
+        payload
+    } = fetchUser
+
+    return {
+        alert,
+        isFetching,
+        isError,
+        isRegistered,
+        payload
+    }
+}
+export default connect(mapStateToProps)(EditPage)
